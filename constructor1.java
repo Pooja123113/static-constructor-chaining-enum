3fc205bfb72a72ca54dc6382b85779f3782f9495
Pooja123113 <85823029+Pooja123113@users.noreply.github.com>
2021-07-28 17:04:02 +0530

    Add files via upload
    static/constructor/enum program.

---
+++ constructor1.java
@@ -0,0 +1,14 @@
+class constructor1
+{
+	
+	String s;
+	int i;
+
+	public static void main(String[]args)
+	{
+	constructor1 c=new constructor1();
+	
+	System.out.println(c.s);
+	System.out.println(c.i);
+	}
+}
